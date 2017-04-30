b8594ab2efe51b9d7c7e7b71d6b2dbb11f582043
Unknown <steven.chou.second@gmail.com>
2017-04-30 15:37:38 +0800

    增加 done callback 於 it block
    當一個 it block 執行成功，才會再執行下一個 it block

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -2,11 +2,18 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-    it('saves a user', () => {
+    it('saves a user', (done) => {
         // 利用 User class 建立 User instance，尚未 insert DB
         const steven = new User({name: 'Steven'});
 
         // 將建立的 instance 存入 DB
-        steven.save();
+        steven.save()
+            .then(() => {
+                // Has steven been saved successfully
+                assert(!steven.isNew);
+
+                // 當 assert return false，就不會往下執行 done()
+                done();
+            });
     });
 });
