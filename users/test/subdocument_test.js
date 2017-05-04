78324250c926ad17b8d2e0a340f5acbe6ab1814d
Unknown <steven.chou.second@gmail.com>
2017-05-04 22:39:58 +0800

    增加移除 subdocument 測試

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -35,4 +35,25 @@ describe('Subdocuments', () => {
                 done();
             });
     });
+
+    it('can remove an exiting subdocument', (done) => {
+        const steven = new User({
+             name: 'Steven',
+             posts: [{ title: 'New Title'}]
+        });
+
+        steven.save()
+            .then(() => User.findOne({ name: 'Steven' }))
+            .then((user) => {
+                // 使用 mongoose 的 remove()，刪除屬性
+                user.posts[0].remove();
+                // return promise obj
+                return user.save();
+            })
+            .then(() => User.findOne({ name: 'Steven' }))
+            .then((user) => {
+                assert(user.posts.length === 0);
+                done();
+            });
+    });
 });
